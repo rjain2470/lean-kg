b1a7a3b4c0502d8d1956bf7e0f6a38b4b871cbd7
Ritik <rjain92682@gmail.com>
2025-06-26 19:57:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ setup(
         'pandas',
         'torch'
     ],
-    author='Your Name', # Replace with your name
-    author_email='your.email@example.com', # Replace with your email
+    author='Ritik Jain'
+    author_email='rjain92682@gmail.com', 
     description='A library for generating and visualizing mathlib4 as a directed multigraph.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/lean-kg', # Replace with your GitHub repo URL
+    url='https://github.com/rjain2470/lean-kg', 
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License', # Choose an appropriate license
+        'License :: OSI Approved :: MIT License', 
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',
