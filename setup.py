687b26943ca23ff9a8ad3f3dc0f3159e8459329d
Ritik <rjain92682@gmail.com>
2025-06-26 23:02:27 -0400

    Changed version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lean-kg',
-    version='0.1.0',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'pykeen',
